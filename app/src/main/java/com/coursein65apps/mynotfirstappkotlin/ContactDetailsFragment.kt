1000dca29565aac0d1552145130c187d45f2f6f5
aleca <aleca2002@yandex.ru>
2021-04-23 11:51:36 +0300

    fix binding

--- app/src/main/java/com/coursein65apps/mynotfirstappkotlin/ContactDetailsFragment.kt
+++ app/src/main/java/com/coursein65apps/mynotfirstappkotlin/ContactDetailsFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.coursein65apps.mynotfirstappkotlin.databinding.ItemFragmentDetailsBinding
 import kotlinx.android.synthetic.main.item_fragment_details.*
 
 class ContactDetailsFragment : Fragment(R.layout.item_fragment_details) {
-    private lateinit var viewBinding: ContactDetailsFragmentBinding
+    private lateinit var viewBinding: ItemFragmentDetailsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding = ContactDetailsFragmentBinding.bind(layoutInflater)
+        viewBinding = ItemFragmentDetailsBinding.inflate(layoutInflater)
         val id: Int? = arguments?.getInt("ID", 0)
         activity?.title = "Детали контактов"
     }
